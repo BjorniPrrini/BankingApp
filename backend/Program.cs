9f8b4107c63a1a38e4533dc0a4b87884e6659cc1
BjorniPrrini <bjorniprrini007@gmail.com>
2026-05-12 19:27:05 +0200

    starting backend

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,27 +1,34 @@
 using Microsoft.EntityFrameworkCore;
 using backend.Data;
+using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
+Env.Load();
+
 builder.Services.AddControllers();
 
-builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(
+        $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
+        $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
+        $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
+        $"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
+        $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")}"
+    )
+);
 
 var app = builder.Build();
 
-if(app.Environment.IsDevelopment()){
+if(app.Environment.IsDevelopment())
+{
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
 
 app.MapControllers();
-
-using(var scope = app.Services.CreateScope()){
-    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    context.Database.EnsureCreated();
-    DbSeeder.Seed(context);
-}
 
 app.Run();
